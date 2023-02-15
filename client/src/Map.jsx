68312904840e81b456ad6b677c5265db8dfbdbd1
mfuke christian kambulu <60177462+mfukechristian@users.noreply.github.com>
2023-02-15 17:49:14 +0200

    Mapping all the stores  in the map
    getting data from json file and creating marker point in the map

--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -1,29 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Icon } from "leaflet";
 import mcDonaldIcon from "./McDonalds-logo.png"; // Import the image file
-
+import stores from "./storesList.json";
 import "leaflet/dist/leaflet.css";
+// import axios from "axios";
 
-const redMarker = new Icon({
+//creating a custom icon for marker
+const mcDoLogo = new Icon({
   iconUrl: mcDonaldIcon,
   iconSize: [50, 30],
 });
 
 const Map = () => {
+  const [showMarkers, setShowMarkers] = useState(false);
+
+  const toggleMarkers = () => {
+    setShowMarkers(!showMarkers);
+  };
+
   return (
-    <div>
+    <div className="main-container">
+      <div className="btn-container">
+        {" "}
+        <button onClick={toggleMarkers}>
+          <img src={mcDonaldIcon} alt="logo" />
+          Mc Donald's Stores <span>({stores.length})</span>
+        </button>
+      </div>
       <MapContainer
         center={[-33.9249, 18.4241]}
-        zoom={13}
+        zoom={11}
         scrollWheelZoom={true}
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={[-33.9249, 18.4241]} icon={redMarker}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {showMarkers &&
+          stores.map((store) => (
+            <Marker
+              key={store.Store}
+              position={[store.Latitude, store.Longitude]}
+              icon={mcDoLogo}
+            >
+              <Popup>
+                <ul>
+                  <li>{store.Store}</li>
+                  <li>
+                    <span>X:</span>
+                    {store.Latitude}
+                  </li>
+                  <li>
+                    <span>Y:</span>
+                    {store.Longitude}
+                  </li>
+                </ul>
+              </Popup>
+            </Marker>
+          ))}
       </MapContainer>
     </div>
   );
